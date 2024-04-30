e59ebffc690c5463cece126108a7088c5e713d31
QUVONCHBEK ABDUROZIQOV <75139080+abduroziqov@users.noreply.github.com>
2024-04-30 15:43:57 -0600

    AuthSystem finished

--- AuthSystem/Controllers/UserController.cs
+++ AuthSystem/Controllers/UserController.cs
@@ -33,6 +33,7 @@ public class UserController : ControllerBase
 	[ProducesResponseType(StatusCodes.Status200OK, Type = typeof(string))]
 	[ProducesResponseType(StatusCodes.Status500InternalServerError)]
 	[ProducesResponseType(StatusCodes.Status400BadRequest)]
+
 	public async Task<IActionResult> Register([FromBody] RegisterRequest request)
 	{
 		var response = await _authenticationService.Register(request);
